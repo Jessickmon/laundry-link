9b29e5157e498c149a3c8e2da3eea7fe8a347f86
dannyzhang1020 <47438725+dannyzhang1020@users.noreply.github.com>
2021-09-19 01:41:12 -0500

    button commit

--- components/washer.js
+++ components/washer.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 
 export default function Washer() {
     const [washers, setWasher] = useState([
@@ -14,8 +14,24 @@ export default function Washer() {
     return (
         <View>
             {washers.map(item => (
-                <View key={item.key}>
-                    <Text style={styles.item}>{item.name}</Text>
+                <View key={item.key} style = {styles.difference}>
+                    <Card>
+                    <Text style={styles.item}>{item.name}
+                        <View style = {styles.align}>
+                            <Card.Actions>
+                                <Button
+                                    onPress={() => {
+                                        alert('Washer timer started');
+                                    }}
+                                    mode="contained"
+                                    color= "#2b487a"
+                                >
+                                    Start Washer
+                                </Button>
+                            </Card.Actions>
+                        </View>
+                    </Text>
+                    </Card>
                 </View>
             ))}
         </View>
@@ -25,11 +41,42 @@ export default function Washer() {
 const styles = StyleSheet.create({
     item: {
         flex: 1,
-        borderColor: 'black',
-        borderWidth: 1,
         textAlign: "left",
-        padding: 30,
         backgroundColor: 'white',
         fontSize: 25,
+        paddingLeft: 50,
+        flexDirection: 'row',
+        paddingTop: 35
+    },
+
+    align: {
+        paddingLeft: 210,
+    },
+
+    difference: {
+        paddingTop: 20
     }
+});
+
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    let fiveMinutes = 60 * 5,
+        display = $('#time');
+    startTimer(fiveMinutes, display);
 });
