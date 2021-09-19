8b409668f4829b63024a790542577b4388fbc3e9
dannyzhang1020 <47438725+dannyzhang1020@users.noreply.github.com>
2021-09-19 02:00:28 -0500

    button commit

--- components/washer.js
+++ components/washer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import Example from 'timer'
 
 export default function Washer() {
     const [washers, setWasher] = useState([
